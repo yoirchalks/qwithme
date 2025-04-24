7c11fc36a7bbf8ff3fc4abf421bc0704fefacc79
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-24 21:35:59 +0300

    added app.ts entry point

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server listening @ port ${port}`);
+});
+
+export default app;
