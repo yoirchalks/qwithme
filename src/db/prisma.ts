161e0e9d97625e657db693a979c9f03cc05a32ba
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-29 21:27:46 +0300

    added get all users

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
@@ -11,4 +15,4 @@ if (process.env.NODE_ENV === 'production') {
   prisma = global.prisma;
 }
 
-export default prisma;
+export { prisma };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,11 @@
 import express, {Request, Response} from 'express'
+import { prisma } from '../db/prisma'
 
 const router = express.Router()
+
+router.get('/', async(req: Request, res: Response)=>{
+    const result = await prisma.patients.findMany()
+    res.send(result)
+})
 
 export default router
