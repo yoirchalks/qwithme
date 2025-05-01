1fd08fc19cf0bea9533568bf128ddd6921b1022d
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-05-01 23:05:26 +0300

    set up ques route

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,10 +1,12 @@
 import {Express} from 'express' 
 import usersRouter from '../routes/users'
 import staffRouter from '../routes/staff'
+import quesRoute from '../routes/ques'
 
 function routes (app: Express): void{
 app.use('/users', usersRouter)
 app.use('/staff', staffRouter)
+app.use('/ques', quesRoute)
 }
 
 export default routes
