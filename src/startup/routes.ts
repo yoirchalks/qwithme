c4c0aec1aef99045508000ff05ee09931259a8b1
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-05-02 15:30:38 +0300

    added signIns route

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -2,11 +2,13 @@ import {Express} from 'express'
 import usersRouter from '../routes/users'
 import staffRouter from '../routes/staff'
 import quesRoute from '../routes/ques'
+import signInRoute from '../routes/signIns'
 
 function routes (app: Express): void{
 app.use('/users', usersRouter)
 app.use('/staff', staffRouter)
 app.use('/ques', quesRoute)
+app.use('/signIns', signInRoute)
 }
 
 export default routes
