afe56b397851ac27e152e57f1f1028365527324b
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-05-04 18:51:29 +0300

    set up rooms router

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,12 +3,14 @@ import usersRouter from '../routes/users'
 import staffRouter from '../routes/staff'
 import quesRoute from '../routes/ques'
 import signInRoute from '../routes/signIns'
+import roomsRouter from '../routes/rooms'
 
 function routes (app: Express): void{
 app.use('/api/users', usersRouter)
 app.use('/api/staff', staffRouter)
 app.use('/api/ques', quesRoute)
 app.use('/api/signIns', signInRoute)
+app.use('api/rooms', roomsRouter)
 }
 
 export default routes
