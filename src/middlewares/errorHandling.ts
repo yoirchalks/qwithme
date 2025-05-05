350407c9da3e8e09a4fab22b797606bfbcb4bba0
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-05-05 09:09:23 +0300

    fixed import in error midlleware

--- src/middlewares/errorHandling.ts
+++ src/middlewares/errorHandling.ts
@@ -1,7 +1,6 @@
 // src/middleware/errorHandler.ts
 import { Request, Response, NextFunction } from "express";
-import { Prisma } from "@prisma/client";
-import { log } from "console";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export function errorHandler(
   err: any,
@@ -9,24 +8,21 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  console.log(err);  
-  
-  if (
-    err instanceof Prisma.PrismaClientKnownRequestError &&
-    err.code === "P2002"
-  ) {
+  console.log(err);
+
+  if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
     const fields = (err.meta?.target ?? []) as string[];
     const errors: Record<string, string> = {};
     fields.forEach((f) => {
       errors[f] = `${f} must be unique.`;
     });
     res.status(409).send(errors);
-    return
+    return;
   }
 
   if (err.status && err.message) {
-   res.status(err.status).send(err.message);
-   return
+    res.status(err.status).send(err.message);
+    return;
   }
 
   res.status(500).send(`Internal server error; ${err}`);
