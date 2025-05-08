0c473f85a9b9140d6a9d23359c5436970ad5045e
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-05-08 19:23:01 +0300

    added PUT method on ques route to call next patient

--- src/routes/ques.ts
+++ src/routes/ques.ts
@@ -6,59 +6,79 @@ import { CustomError } from "../utils/CustomError";
 
 const router = express.Router();
 
-router.get('/', async(req: Request, res: Response)=>{
-    
-    const roomId = req.query.roomId as string || null 
-    let cleanedRoomId;
-    if(roomId && /^[1-9]\d*$/.test(roomId)){
-        cleanedRoomId = parseInt(roomId)
-    }
-    else{
-        res.status(400).send(`room id can only be an integer`)
-        return
-    }
-    let results;
-    if(cleanedRoomId){
-        results = await prisma.ques.findMany({
-            where: {
-                room_id: cleanedRoomId
-            }
-        })
-    }
-    else{
-        results = await prisma.ques.findMany()
-    }
+router.get("/", async (req: Request, res: Response) => {
+  const roomId = (req.query.roomId as string) || null;
+  let cleanedRoomId;
+  if (roomId && /^[1-9]\d*$/.test(roomId)) {
+    cleanedRoomId = parseInt(roomId);
+  } else {
+    res.status(400).send(`room id can only be an integer`);
+    return;
+  }
+  let results;
+  if (cleanedRoomId) {
+    results = await prisma.ques.findMany({
+      where: {
+        room_id: cleanedRoomId,
+      },
+    });
+  } else {
+    results = await prisma.ques.findMany();
+  }
 
-    res.send(results)
-})
+  res.send(results);
+});
 
-router.post('/', async(req: Request, res: Response)=>{
-    
-})
+router.post("/", async (req: Request, res: Response) => {});
 
-router.get('/:id', async(req: Request, res: Response)=>{
-     const id = parseInt(req.params.id);
-      const que = await prisma.ques.findUnique({
-        where: {
-          id: id,
-        },
-      });
-    
-      if (!que) {
-        res.status(404).send(`no que found with id ${req.params.id}`);
-        return;
-      }
-      res.send(que);
-    
-})
+router.get("/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const que = await prisma.ques.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-router.put('/:id', async(req: Request, res: Response)=>{
-    const result = validateQue('post', req.body);
-    if(!result.success) throw new CustomError(400, `${result.field} is ${result.message?.toLowerCase()}`)
-    
-    const {patientId, roomId, staffId} = req.body;
+  if (!que) {
+    res.status(404).send(`no que found with id ${req.params.id}`);
+    return;
+  }
+  res.send(que);
+});
 
-    const queNumber = prisma.ques
-})
+router.put("", async (req: Request, res: Response) => {
+  const que = await prisma.ques.findFirst({
+    where: {
+      status: "waiting",
+    },
+  });
+
+  if (!que) {
+    res.send("no patients waiting");
+    return;
+  }
+
+  const nextPatient = await prisma.ques.update({
+    where: {
+      id: que.id,
+    },
+    data: {
+      status: "being_seen",
+      started_time: new Date(),
+    },
+  });
+
+  const prevPatient = await prisma.ques.updateMany({
+    where: {
+      id: que.id - 1,
+    },
+    data: {
+      status: "done",
+      completed_time: new Date(),
+    },
+  });
+  res.send(nextPatient);
+  console.log(prevPatient);
+});
 
 export default router;
